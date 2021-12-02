e00c0cf8f4895ed03af3c44c1eb63bde8aba86f0
Sara Hackle <Shackle@ivytech.edu>
2021-12-02 14:16:04 -0600

    Initial Commit, debugged page

---
+++ scripts/Metals.js
@@ -0,0 +1,21 @@
+import { getMetals } from "./database.js";
+
+const metals = getMetals();
+
+document.addEventListener("change", (event) => {});
+
+export const Metals = () => {
+  let html = "<ul>";
+
+  // Use .map() for converting objects to <li> elements
+  const listItems = metals.map((metal) => {
+    return `<li>
+            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
+        </li>`;
+  });
+
+  html += listItems.join("");
+  html += "</ul>";
+
+  return html;
+};
